9af93181a38993501eff2ae649b1147b91d4a606
yachika <01fe24bca222@kletech.ac.in>
2025-10-29 09:11:38 +0530

    Addition and Substraction

---
+++ addsub.py
@@ -0,0 +1,11 @@
+# Get input from the user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Perform addition and subtraction
+addition = a + b
+subtraction = a - b
+
+# Display the results
+print("Addition of two numbers =", addition)
+print("Subtraction of two numbers =", subtraction)
